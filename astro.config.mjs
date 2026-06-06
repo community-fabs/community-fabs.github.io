8e919fb2d3471ccce2105a59a257628bd7254e56
Greg Willard <gwillard@r3pwn.com>
2026-06-06 00:29:58 -0400

    feat: add doc topics, add basic page structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,55 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://community-fabs.github.io',
-    integrations: [
-		starlight({
-        	title: 'My Docs',
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/community-fabs'
-				}
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{
-							label: 'Example Guide',
-							slug: 'guides/example'
-						},
-					],
-				},
-				{
-					label: 'Reference',
-					items: [
-						{
-							autogenerate: {
-								directory: 'reference'
-							}
-						}
-					],
-				},
-			],
-		})
-	],
+  site: 'https://community-fabs.github.io',
+  integrations: [
+    starlight({
+      title: 'Community Fabs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/community-fabs'
+        }
+      ],
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: 'Overview',
+            link: '/',
+            icon: 'open-book',
+            items: ['index'],
+          },
+          {
+            label: 'Concepts',
+            link: '/concepts/todo',
+            icon: 'open-book',
+            items: ['concepts/todo'],
+          },
+          {
+            label: 'SDKs',
+            link: '/sdks',
+            icon: 'information',
+            items: [
+              'sdks',
+              {
+                label: 'JavaScript',
+                items: [{ autogenerate: { directory: 'sdks/js' } }],
+                collapsed: true,
+              },
+              {
+                label: '.NET',
+                items: [{ autogenerate: { directory: 'sdks/dotnet' } }],
+                collapsed: true,
+              },
+            ],
+          },
+        ]),
+      ],
+    })
+  ],
 });
---
+++ src/content/docs/concepts/todo.md
@@ -0,0 +1,6 @@
+---
+title: Concepts
+#description: A guide in my new Starlight docs site.
+---
+
+This doc will eventually have some more information on PlayFab concepts that may require additional explanation.
