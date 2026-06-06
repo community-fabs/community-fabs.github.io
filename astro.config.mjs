b33cdd3964ece545d802a57f5d6d8a9148bb8892
Greg Willard <gwillard@r3pwn.com>
2026-06-05 23:42:01 -0400

    chore: format config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,39 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://community-fabs.github.io',
-	integrations: [
+    site: 'https://community-fabs.github.io',
+    integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        	title: 'My Docs',
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/community-fabs'
+				}
+			],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{
+							label: 'Example Guide',
+							slug: 'guides/example'
+						},
 					],
 				},
 				{
 					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					items: [
+						{
+							autogenerate: {
+								directory: 'reference'
+							}
+						}
+					],
 				},
 			],
-		}),
+		})
 	],
 });
